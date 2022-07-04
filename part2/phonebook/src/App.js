8fe5911a701c0a4ed95ddbecddc0be247679b439
MinMag <magnusoco@gmail.com>
2022-07-03 20:00:29 -0500

    2.19

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,6 +5,18 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import PersonService from './services/contacts'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='notif'> 
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
   
@@ -17,6 +29,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+  const [message, setMessage] = useState('message')
 
 
   const handleNewPerson = (event) => {
@@ -31,6 +44,10 @@ const App = () => {
     } else {
       PersonService.create(newPerson)
       .then((response) => {
+      setMessage(`Added ${response.data.name}`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
       setPersons(persons.concat(response.data))
       setNewName('')
       setNewNumber('')
@@ -68,6 +85,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={message}/>
       <SearchBar search={search} handle={handleSearchChange} />
 
       <h2>add a new contact</h2>
